2d3a51ffb4e8f312432a90bea840a869e3c038ac
vinix38 <vinceguichard04@gmail.com>
2021-05-31 21:24:47 +0200

    ça progresse

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-function prin(nb) {
-    console.log("slt");
-    var i, f
-    for (i = 1; i <= nb; i++) {
-        f = document.createElement("img")
-        f.setAttribute("src", "images/"+i+".png")
-        f.setAttribute("class", "image")
-        f.setAttribute("id", i)
+const NOMBRE_IMAGES = 7
 
-        document.getElementById("liste").appendChild(f)
-    }
-}
+//===========================================//
+
+function prin() {
+    nb = NOMBRE_IMAGES;
+    var i, f, li = Math.ceil(nb / 3);
+
+    for (i = 1; i <= li; i++) {
+        f = document.createElement("div");
+
+        f.setAttribute("class", "ligne");
+        f.setAttribute("id", "li_" + i);
+
+        document.getElementById("liste").appendChild(f);
+    };
+
+    for (i = 1; i <= nb; i++) {
+        f = document.createElement("img");
+
+        f.setAttribute("src", "images/" + i + ".png");
+        f.setAttribute("class", "image");
+        f.setAttribute("id", "img_" + i);
+
+        document.getElementById("li_" + Math.ceil(i / 3)).appendChild(f);
+    };
+};
