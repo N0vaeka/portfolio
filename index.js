db7b44b7d5b843ab05220fefb0bc1eee359b4ca9
vinix38 <vinceguichard04@gmail.com>
2021-06-05 14:14:56 +0200

    ça commence à ressembler à quelque chose

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const NOMBRE_IMAGES = 7
 
 //===========================================//
 
-function prin() {
-    nb = NOMBRE_IMAGES;
-    var i, f, li = Math.ceil(nb / 3);
+function affImages() {
+    var i, f, li = Math.ceil(NOMBRE_IMAGES / 3);
 
     for (i = 1; i <= li; i++) {
         f = document.createElement("div");
@@ -15,13 +14,31 @@ function prin() {
         document.getElementById("liste").appendChild(f);
     };
 
-    for (i = 1; i <= nb; i++) {
+    for (i = 1; i <= NOMBRE_IMAGES; i++) {
         f = document.createElement("img");
 
         f.setAttribute("src", "images/" + i + ".png");
         f.setAttribute("class", "image");
         f.setAttribute("id", "img_" + i);
+        f.setAttribute("alt", "Oups! Image n°" + i)
 
         document.getElementById("li_" + Math.ceil(i / 3)).appendChild(f);
+    };
+};
+
+function question() {
+    var coll = document.getElementsByClassName("pli");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            };
+        } );
     };
 };
